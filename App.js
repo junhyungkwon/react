db45e045871da15e965be575f851aec0ad71d533
junhyung kwon <78072931+junhyungkwon@users.noreply.github.com>
2022-06-23 22:29:53 +0900

    Add files via upload
    Button, alert study

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ import Box from './Box'
 function App() {
   return (
    <main>
-    <Box/>
-    <Box/>
+    <Box name = "html" num= "1"/>
+    <Box name = "css" num= "2"/>
+    <Box name = "js" num= "3"/>
+    <Box name = "react" num= "4"/>
    </main>
   );
 }
--- Box.js
+++ Box.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-function Box(){
-    return(
+function Box(props){
+  const clickMe = ()=>{
+    alert("강의를 끝맞췄습니다!")
+  
+  }
+  return(
         <div className='Box'>
-      Box1
-      junhyung
-     
+      Box{props.num} 
+      {props.name}
+      <button onClick={clickMe}>수강하기</button>
+      
     </div>
     )
 
-}
+    
+  }
 export default Box;
