624dec6aa457ebad251fbc2665dc943c1bca0108
junhyung kwon <78072931+junhyungkwon@users.noreply.github.com>
2022-06-29 17:44:09 +0900

    Add files via upload
    component modifi

--- App.js
+++ App.js
@@ -1,25 +1,17 @@
-
 import './App.css';
+import Hello from './component/Hello';
+import Welcome from './component/Welcome';
+
 
 function App() {
-  const name = "jun1";
-  const naver = {
-    name : "네이버",
-    URL : "https://m.naver.com"
-  };
+ 
+
   
   return (
     <div className=' App'>
-      <h1
-      style={{
-        color: "#f0f",
-        backgroundColor: "green",
-
-      }}
-      >
-         hello, {name}, <p>{3+12}</p>
-      </h1>
-      <a href={naver.URL}>{naver.name}</a>
+      <Hello />
+      <Welcome />
+     
     </div>
 
   
